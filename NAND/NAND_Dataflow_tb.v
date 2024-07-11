fedd20f30b5e07f5f0be8cbe9dc336d44bfa755a
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-11 23:26:37 +0530

    Create NAND_Dataflow_tb.v

---
+++ NAND/NAND_Dataflow_tb.v
@@ -0,0 +1,17 @@
+//NAND Gate Testbench
+//Dataflow modelling
+
+module NAND_Dataflow_tb();
+reg a,b;
+wire y;
+
+NAND_Dataflow uut(.a(a), .b(b), .y(y));
+
+initial begin
+a = 0; b = 0; #100;
+a = 0; b = 1; #100;
+a = 1; b = 0; #100;
+a = 1; b = 1; #100;
+$stop;
+end
+endmodule
