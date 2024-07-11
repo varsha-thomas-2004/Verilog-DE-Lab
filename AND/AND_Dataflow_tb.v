50bec6e0c8756ace6a92cc818e9f4beb58c040ab
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-11 20:53:32 +0530

    Update AND_Dataflow_tb.v

--- AND/AND_Dataflow_tb.v
+++ AND/AND_Dataflow_tb.v
@@ -9,9 +9,9 @@ AND_Dataflow uut(.a(a), .b(b), y(y));
 
 initial begin
 a = 0; b = 0; #100;
-a = 0; b = 0; #100;
-a = 0; b = 0; #100;
-a = 0; b = 0; #100;
+a = 0; b = 1; #100;
+a = 1; b = 0; #100;
+a = 1; b = 1; #100;
 $stop;
 end 
 endmodule
