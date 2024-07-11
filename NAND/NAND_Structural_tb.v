dca08c73167fed834b0e6928c9598f5539d2cc9a
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-11 23:24:29 +0530

    Create NAND_Structural_tb.v

---
+++ NAND/NAND_Structural_tb.v
@@ -0,0 +1,17 @@
+//NAND Gate Testbench
+//Structural modelling
+
+module NAND_Structural_tb();
+reg a,b;
+wire y;
+
+NAND_Structural uut(.a(a), .b(b), .y(y));
+
+initial begin
+a = 0; b = 0; #100;
+a = 0; b = 1; #100;
+a = 1; b = 0; #100;
+a = 1; b = 1; #100;
+$stop;
+end
+endmodule
