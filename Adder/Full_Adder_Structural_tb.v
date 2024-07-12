29c4358b5f7dfd4f570158b273c703e97f5318bf
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-12 11:02:51 +0530

    Create Full_Adder_Structural_tb.v

---
+++ Adder/Full_Adder_Structural_tb.v
@@ -0,0 +1,20 @@
+//Full Adder Structural Testbench
+
+module Full_Adder_Structural_tb();
+reg a,b,c;
+wire sum,carry;
+
+Full_Adder_Structural uut(.a(a), .b(b), .c(c), .sum(sum), .carry(carry));
+
+initial begin
+a = 0; b = 0; c = 0; #100;
+a = 0; b = 0; c = 1; #100;
+a = 0; b = 1; c = 0; #100;
+a = 0; b = 1; c = 1; #100;
+a = 1; b = 0; c = 0; #100;
+a = 1; b = 0; c = 1; #100;
+a = 1; b = 1; c = 0; #100;
+a = 1; b = 1; c = 1; #100;
+$stop;
+end
+endmodule
